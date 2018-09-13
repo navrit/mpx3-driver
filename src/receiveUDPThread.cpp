c4e5f9a62b6af14ebcae737ca3be04ee3667feb3
Navrit Bal <navrit666@gmail.com>
2018-09-13 13:52:09 +0200

    Better logic for failed connection

--- src/receiveUDPThread.cpp
+++ src/receiveUDPThread.cpp
@@ -13,11 +13,12 @@ int main() {
   if (set_scheduler() != 0) {
       return false;
   }
-
   if (!initSpidrController("127.0.0.1", TCPPort)) {
-      return 1;
+      return false;
   }
-  connectToDetector();
+  if (!connectToDetector()) {
+      return false;
+  }
   initSocket(); //! No arguments --> listens on all IP addresses
                 //! Arguments --> IP address as const char *
   initDetector();
@@ -299,10 +300,10 @@ bool initSpidrController(const char * IPAddr, int port)
     int a, b, c, d;
     sscanf(IPAddr, "%d.%d.%d.%d", &a, &b, &c, &d);
     spidrcontrol = new SpidrController(a, b, c, d, port);
-    if (spidrcontrol->isConnected()) {
+    if (spidrcontrol != nullptr) {
         return true;
     } else {
-        std::cout << BOLD(FRED("SpidrController not connected, start the emulator or connect a SPIDR.\n"));
+        std::cout << BOLD(FRED("Could not make SpidrController, start the emulator or connect a SPIDR.\n"));
         return false;
     }
 }
