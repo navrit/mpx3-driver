575a3390937db70ebb1dd738a946316d7106ad58
Navrit Bal <navrit666@gmail.com>
2018-10-16 13:54:09 +0200

    Number of triggers gets downcast into an int later anyway

--- src/configs.h
+++ src/configs.h
@@ -11,7 +11,7 @@ struct Config{
     const int trig_length_us = 250;   //! [us]
     const int trig_deadtime_us = 250; //! [us]
 
-    const uint64_t nr_of_triggers = 10000;
+    const int nr_of_triggers = 10000;
     const int continuousRW_frequency = 2000; //! [Hz]
     const bool readoutMode_sequential = false;
 
@@ -23,8 +23,8 @@ struct Config{
 };
 
 struct NetworkSettings{
-    const std::string socketIPAddr = "127.0.0.10";
-    // const std::string socketIPAddr = "192.168.100.10";
+//    const std::string socketIPAddr = "127.0.0.10";
+     const std::string socketIPAddr = "192.168.100.10";
     // const std::string socketIPAddr = "192.168.1.10";
     const int TCPPort = 50000;
     const int portno = 8192;
